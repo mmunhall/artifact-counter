0ba57f72ee181c6c2c4115febe6f1a0d09ca64a2
Mike Munhall <munhall.mike@gmail.com>
2015-02-25 14:00:00 -0700

    Added buttons.

--- artifact_counter/artifact_counter.ino
+++ artifact_counter/artifact_counter.ino
@@ -1,33 +1,68 @@
-
-// These values are configurable and definee the pins used on the Arduino.
+const int MOMENTARY_PINS[] = {2, 3, 4, 5, 6, 7};
 const int PIN_SERIAL = 8;
 const int PIN_LATCH = 9;
 const int PIN_CLOCK = 10;
+const long BOUNCE_DELAY = 50;
+
+// For debouncing the momentary-on buttons.
+int BUTTON_STATES[sizeof(MOMENTARY_PINS)];
+int LAST_BUTTON_STATES[sizeof(MOMENTARY_PINS)];
+long LAST_BUTTON_DEBOUNCE_TIMES[sizeof(MOMENTARY_PINS)];
 
 // Integer values for the seven-segment LEDS for each digit (0-9). The ordinal position represents the binary value for that digit.
 const int SEVEN_SEG_BINARY[] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
 
 // The initial value of each seven-segment LED.
-int LED_VALUES[] = {0, 1, 2};
+int ledValues[] = {0, 0, 0};
 
-const int NUM_DIGITS = sizeof(LED_VALUES)/sizeof(int);
+// We can be in one of two modes: COUNTER or CALCULATOR.
+// TODO: Use an enum for MODES
+char* MODES[] = {"COUNTER", "CALCULATOR"};
+
+// The default mode is COUNTER
+char* currentMode = MODES[0];
 
 void setup() {
   Serial.begin(9600);
+  for (int i=0; i<sizeof(MOMENTARY_PINS); i++) {
+    pinMode(MOMENTARY_PINS[i], INPUT_PULLUP);
+    BUTTON_STATES[i] = HIGH;
+    LAST_BUTTON_STATES[i] = HIGH;
+    LAST_BUTTON_DEBOUNCE_TIMES[i] = 0;
+  }
   pinMode(PIN_SERIAL, OUTPUT);
   pinMode(PIN_LATCH, OUTPUT);
   pinMode(PIN_CLOCK, OUTPUT);
+  
+  render();
 }
 
 void loop() {
-  render();
-  delay(10000);
+  for (int i=0; i<sizeof(MOMENTARY_PINS); i++) {
+    int reading = digitalRead(MOMENTARY_PINS[i]);
+    
+    if (reading != LAST_BUTTON_STATES[i]) {
+      LAST_BUTTON_DEBOUNCE_TIMES[i] = millis();
+    } 
+  
+    if ((millis() - LAST_BUTTON_DEBOUNCE_TIMES[i]) > BOUNCE_DELAY) {
+      if (reading != BUTTON_STATES[i]) {
+        BUTTON_STATES[i] = reading;
+  
+        if (BUTTON_STATES[i] == LOW) {
+          Serial.println(i);
+        }
+      }
+    }
+    
+    LAST_BUTTON_STATES[i] = reading;
+  }
 }
 
 void render() {
   digitalWrite(PIN_LATCH, LOW);
-  for (int i = NUM_DIGITS - 1; i >= 0; i--) {
-    int shiftValue = SEVEN_SEG_BINARY[LED_VALUES[i]];
+  for (int i = 2; i >= 0; i--) {
+    int shiftValue = SEVEN_SEG_BINARY[ledValues[i]];
     shiftOut(PIN_SERIAL, PIN_CLOCK, MSBFIRST, shiftValue);
   }
   digitalWrite(PIN_LATCH, HIGH);
